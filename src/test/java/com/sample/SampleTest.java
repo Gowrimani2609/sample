655532add1a0c2b93494d1c392eb39f0dd4fca7d
GowriMani <gowrimani2609@gmail.com>
2021-09-07 10:40:13 +0530

    sample

---
+++ src/test/java/com/sample/SampleTest.java
@@ -0,0 +1,12 @@
+package com.sample;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("SampleTest");
+
+	}
+
+}
